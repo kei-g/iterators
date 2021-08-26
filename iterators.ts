04d0d2e66f4ddfc4e8f04e994b36fea8009161b0
kei-g <km.8k6ce+github@gmail.com>
2021-08-26 21:39:24 +0900

    :sparkles: Add async concatenated iterable
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- iterators.ts
+++ iterators.ts
@@ -1,9 +1,47 @@
-export class ConcatenatedIterable<T> implements Iterable<T> {
-  constructor(private readonly first: Iterable<T>, private readonly second: Iterable<T>) {
+export type BothIterable<T> = AsyncIterable<T> & Iterable<T>
+
+export type EitherIterable<T> = AsyncIterable<T> | Iterable<T>
+
+export class ConcatenatedAsyncIterable<T> implements AsyncIterable<T> {
+  static of<T>(...source: (EitherIterable<T>)[]): AsyncIterable<T> {
+    return new ConcatenatedAsyncIterable(source)
+  }
+
+  constructor(private readonly source: EitherIterable<EitherIterable<T>>) {
+  }
+
+  [Symbol.asyncIterator](): AsyncIterator<T> {
+    return async function* (source: EitherIterable<EitherIterable<T>>) {
+      for await (const iterable of source)
+        for await (const value of iterable)
+          yield value
+    }(this.source)
+  }
+}
+
+export class ConcatenatedIterable<T> implements AsyncIterable<T>, Iterable<T> {
+  static of<T>(...source: Iterable<T>[]): Iterable<T> {
+    return new ConcatenatedIterable<T>(source)
+  }
+
+  private readonly source: Iterable<Iterable<T>>
+
+  constructor(sourceOrFirst: Iterable<Iterable<T>> | Iterable<T>, second?: Iterable<T>) {
+    this.source = typeof second === 'undefined'
+      ? sourceOrFirst as Iterable<Iterable<T>>
+      : [sourceOrFirst as Iterable<T>, second]
+  }
+
+  [Symbol.asyncIterator](): AsyncIterator<T> {
+    return new ConcatenatedAsyncIterable(this.source)[Symbol.asyncIterator]()
   }
 
   [Symbol.iterator](): Iterator<T> {
-    return new ConcatenatedIterator(this.first[Symbol.iterator](), this.second[Symbol.iterator]())
+    return function* (source: Iterable<Iterable<T>>) {
+      for (const iterable of source)
+        for (const value of iterable)
+          yield value
+    }(this.source)
   }
 }
 
